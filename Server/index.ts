2aabf121e993c1ee8990df65112a008edd6cbd1c
s334047 <s334047@studenti.polito.it>
2024-11-02 12:43:07 +0100

    adjustments

--- Server/index.ts
+++ Server/index.ts
@@ -1,11 +1,8 @@
-import bcrypt from 'bcrypt';
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import { DocumentDescription } from './Components/DocumentDescription.js';
 import Dao from './Dao/daoStory1.js';
-import { db } from './DB/db.js';
-import { seedUsers } from './seedUsers.js';
 
 const dao = new Dao();
 
@@ -21,9 +18,6 @@ const corsOption = {
 };
 app.use(cors(corsOption));
 
-// Seed initial users into the database
-seedUsers();
-
 /** Story 1 routes */
 app.post('/api/documents', async (req: any, res: any) => {
     try {
@@ -32,43 +26,6 @@ app.post('/api/documents', async (req: any, res: any) => {
         res.status(201).json({ message: "Document created successfully" });
     } catch (error) {
         res.status(503).json({ error: error.message });
-    }
-});
-
-// Login route
-app.post('/api/auth/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        db.get(`SELECT * FROM User WHERE Username = ?`, [username], async (err, row: {
-            Role: any;
-            Username: any;
-            Id: any; Password: string 
-}) => {
-            if (err) {
-                return res.status(500).json({ error: "Internal Server Error" });
-            }
-
-            if (!row) {
-                return res.status(401).json({ error: "Invalid username or password" });
-            }
-
-            const isMatch = await bcrypt.compare(password, row.Password);
-            if (!isMatch) {
-                return res.status(401).json({ error: "Invalid username or password" });
-            }
-
-            // Create user session or JWT token
-            const user = {
-                id: row.Id,
-                username: row.Username,
-                role: row.Role,
-            };
-
-            res.json({ message: 'Login successful', user });
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Something went wrong" });
     }
 });
 
