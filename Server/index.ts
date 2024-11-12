e0dcc9b8a1659b055dc380273561c82867d07e46
Enrico Gioseffi <s334022@studenti.polito.it>
2024-11-12 16:00:23 +0100

    try different settings

--- Server/index.ts
+++ Server/index.ts
@@ -200,7 +200,3 @@ export { app };
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
